34463fe6b299ad90c4492fec3ccb9ab98ed3cf1c
TestSubject528491 <chrisharvey2pi@gmail.com>
2013-07-24 18:32:27 -0400

    updated typo.html; rearranged selectors in typo.less and mods.less

--- core/scripts/all.js
+++ core/scripts/all.js
@@ -36,20 +36,16 @@ $(document).ready(function() {
 	The only exception to this rule is to use JS to automate repetitive actions. For instance in an unordered list, if every line item needs to have a certain class, you can save time by writing a jQuery command to add the class to every line item.
 	*/
 	
-	/** Automated actions --- add 'real' CSS classes to elements */
+	/* Automated actions --- add 'real' CSS classes to elements */
 	$('.list-action a').addClass('up');
 	
 	$('table thead, table tfoot, table th').addClass('header');
-	
 	$('.table-list thead, .table-list tfoot, .table-list th').addClass('header');
 	$('.table-list tbody').addClass('body');
-	
 	$('.table-bars th').addClass('header');
 	//no body for table-bars
-	
 	$('.table-grid th').addClass('header');
 	$('.table-grid tbody').addClass('body');
-	
 	
 	/** JavaScript 'grabber' classes --- only here for jQuery selectors */
 	$('a.up, a.dn').addClass('js-link');
@@ -58,57 +54,10 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
+	/* BLOCK ELEMENTS */
 	
-	// hyperlinks
-	$('.js-link[href]').hover(function() {
-		$(this).toggleClass('up dn');
-		$(this).removeClass('active');
-	});
-	$('.js-sp[href]').hover(function() {
-		$(this).toggleClass('sp-up sp-dn');
-		$(this).removeClass('active');
-	});
-	
-	$('a[href]').mousedown(function() {
-		$(this).addClass('active');
-	});
-	$('a[href]').mouseup(function () {
-		$(this).removeClass('active');
-	});
-	
-	// box hyperlinks
-	$('.js-block-link').hover(function() {
-		$(this).toggleClass('block-up block-dn',t_ani);
-	});
-	$('.js-block-link').mousedown(function() {
-		$(this).addClass('block-active');
-	});
-	$('.js-block-link').mouseup(function () {
-		$(this).removeClass('block-active');
-	});
-		
-	// hyperlinks in main navigation bars (banner.html)
-	$('.hub-nav a').hover(function() {
-		$(this).parent().toggleClass('pos neg',t_ani);
-	});
-	
-	// mods
-	showOnHover($('.example, .demo'));
-	addLabel($('.example'), 'example');
-	addLabel($('.demo'), 'demo');
-		
-	// code spans and code blocks
-	$('.lang-html').attr('title','HTML');
-	$('.lang-css').attr('title','CSS');
-	$('.lang-less').attr('title','LESS');
-	$('.lang-js').attr('title','JavaScript');
-	$('.lang-jq').attr('title','jQuery');
-	
-	
-	/**
-	color tables' rows and columns alternating colors 
-	remember, jQuery's :even and :odd selectors are 0-based (0 is first)
-	*/
+	/** Colors tables' rows and columns alternating colors. 
+	remember, jQuery's :even and :odd selectors are 0-based (0 is first) */
 	var jQobj;
 	for (var i=0; i<$('.table-list .body').size(); i++) {
 		jQobj = $($('.table-list .body').toArray()[i]);
@@ -131,34 +80,65 @@ $(document).ready(function() {
 		jQobj.children('td:odd').addClass('column-even');
 	}
 	
-	// special icons
+	/* INLINE ELEMENTS */
+	$('[datetime]').attr('title',function() {
+		return $(this).attr('datetime');
+	});
+	/* hyperlinks */
+	$('.js-link[href]').hover(function() {
+		$(this).toggleClass('up dn');
+		$(this).removeClass('active');
+	});
+	$('.js-sp[href]').hover(function() {
+		$(this).toggleClass('sp-up sp-dn');
+		$(this).removeClass('active');
+	});
+	
+	$('a[href]').mousedown(function() {
+		$(this).addClass('active');
+	});
+	$('a[href]').mouseup(function () {
+		$(this).removeClass('active');
+	});
+	
+	/* MODS */
+	showOnHover($('.example, .demo'));
+	addLabel($('.example'), 'example');
+	addLabel($('.demo'), 'demo');
+	/* block hyperlinks */
+	$('.js-block-link').hover(function() {
+		$(this).toggleClass('block-up block-dn',t_ani);
+	});
+	$('.js-block-link').mousedown(function() {
+		$(this).addClass('block-active');
+	});
+	$('.js-block-link').mouseup(function () {
+		$(this).removeClass('block-active');
+	});
+	/* hyperlinks in main navigation bars */
+	$('.hub-nav a').hover(function() {
+		$(this).parent().toggleClass('pos neg',t_ani);
+	});
+	/* code */
+	$('.lang-html').attr('title','HTML');
+	$('.lang-css').attr('title','CSS');
+	$('.lang-less').attr('title','LESS');
+	$('.lang-js').attr('title','JavaScript');
+	$('.lang-jq').attr('title','jQuery');
+	/* math */
+	$('.latex').attr('title',function() {
+		return $(this).text();
+	});
+	/* special icons */
 	$('.icon-special').addClass('icon-special-clear');
 	$('.icon-special').hover(function() {
 		$(this).toggleClass('icon-special-clear',t_ani);
 	});
-	
-	// inline elements
-	$('.latex').attr('title',function() {
-		return $(this).text();
-	});
-	$('[datetime]').attr('title',function() {
-		return $(this).attr('datetime');
-	});
-
-	
-	// footer-hub
-	/*$('.footer-hub').addClass('footer-hub-collapse');
-	$('.footer-hub').hover(function() {
-		$(this).toggleClass('footer-hub-collapse',t_ani);
-	});*/
-	
-	
-	// an attempt to fix the cursive headings of the hub pages
+	/** an attempt to fix the cursive headings of the hub pages 
+	EDIT: Temporary fix available at the bottom of each hub page. */
 //	while($('.folio .h-folio').width > $('.folio').width) {
 //		$('.folio .h-folio').css('font-size', (parseInt($('.folio .h-folio').css('font-size')) - 1) + "px" );
 //	}
-// EDIT: Temporary fix available at the bottom of each hub page.
-	
 });
 
 
