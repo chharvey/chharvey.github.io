04814219b283293b437f9ca975f1e24f405e5ede
TestSubject528491 <chrisharvey2pi@gmail.com>
2013-07-26 21:57:58 -0400

    updated .table classes, made a few mathjax config changes

--- core/scripts/all.js
+++ core/scripts/all.js
@@ -14,7 +14,8 @@ function addLede(jQobj, html) {
 	addSomethingTo(jQobj, 'span', 'lede', html);
 }
 function addLabel(jQobj, html) {
-	addSomethingTo(jQobj, 'div', 'label', html);
+//USE THE CSS ':before' element!!!
+//	addSomethingTo(jQobj, 'div', 'label', html);
 }
 function showOnHover(container) {
 	container.wrapInner(newElem('div').addClass('js-wrapper invisible'));
@@ -22,31 +23,18 @@ function showOnHover(container) {
 		$(this).find('.js-wrapper').toggleClass('invisible',t_ani);
 	});
 }
-
 $(document).ready(function() {
 	/*
 	Adds classes to necessary elements.
-	These methods are NOT meant for extending classes! For extending CSS classes, either use includes (redundant properties) or extends (multiple selectors), or some other method of inheritance.
+	These methods are NOT meant for extending classes! For extending CSS classes, either use includes (redundant properties) or extends (multiple selectors), or some other method of inheritance in the stylesheet.
 	These methods are meant for using JS to 'grab' certain classes for behavioral reasons.
 	For example, the "js-link" class enables JS to toggle the subclasses "up" and "dn". In the HTML, you can just apply the class
 		<div class="up">
 		and thus JS will add the class "js-link" to the div. The compiled HTML will output <div class="up js-link"> and now jQuery can 'grab' the element with class "js-link" and toggle "up" and "down".
 		**The classes specific to JavaScript and not needed for CSS styling shall be prefixed with "js-" **
 	If trying to decide whether to include a JS method to add a class, think, "does JS need to use the class or is only used for CSS?" If JS doesn't need it, you can find a better way to get what you want using CSS.
-	The only exception to this rule is to use JS to automate repetitive actions. For instance in an unordered list, if every line item needs to have a certain class, you can save time by writing a jQuery command to add the class to every line item.
+	The only exception to this rule is to use JS to automate repetitive commands. For instance in an unordered list, if every line item needs to have a certain class, you can save time by writing a jQuery command to add the class to every line item.
 	*/
-	
-	/* Automated actions --- add 'real' CSS classes to elements */
-	$('.list-action a').addClass('up');
-	
-	$('table thead, table tfoot, table th').addClass('header');
-	$('.table-list thead, .table-list tfoot, .table-list th').addClass('header');
-	$('.table-list tbody').addClass('body');
-	$('.table-bars th').addClass('header');
-	//no body for table-bars
-	$('.table-grid th').addClass('header');
-	$('.table-grid tbody').addClass('body');
-	
 	/** JavaScript 'grabber' classes --- only here for jQuery selectors */
 	$('a.up, a.dn').addClass('js-link');
 	$('a.sp-up, a.sp-dn').addClass('js-sp');
@@ -57,33 +45,29 @@ $(document).ready(function() {
 	/* BLOCK ELEMENTS */
 	
 	/** Colors tables' rows and columns alternating colors. 
-	remember, jQuery's :even and :odd selectors are 0-based (0 is first) */
-	var jQobj;
-	for (var i=0; i<$('.table-list .body').size(); i++) {
-		jQobj = $($('.table-list .body').toArray()[i]);
-		jQobj.children('tr:even').addClass('row-odd');
-		jQobj.children('tr:odd').addClass('row-even');
-	}
-	for (i=0; i<$('.table-bars tr').size(); i++) {
-		jQobj = $($('.table-bars tr').toArray()[i]);
-		jQobj.children('td:even').addClass('column-odd');
-		jQobj.children('td:odd').addClass('column-even');
-	}
-	for (i=0; i<$('.table-grid .body').size(); i++) {
-		jQobj = $($('.table-grid .body').toArray()[i]);
-		jQobj.children('tr:even').addClass('row-odd');
-		jQobj.children('tr:odd').addClass('row-even');
-	}
-	for (i=0; i<$('.table-grid tr').size(); i++) {
-		jQobj = $($('.table-grid tr').toArray()[i]);
-		jQobj.children('td:even').addClass('column-odd');
-		jQobj.children('td:odd').addClass('column-even');
-	}
+	remember, jQuery's :even and :odd selectors are 0-based (0 is first),
+	so the first row (row 0) should be given class 'row-odd' */
+	$('.table-list .body').each(function () {
+		$(this).children('tr:even').addClass('row-odd');
+		$(this).children('tr:odd').addClass('row-even');
+	});
+	$('.table-bars .body tr').each(function() {
+		$(this).children('td:even').addClass('column-odd');
+		$(this).children('td:odd').addClass('column-even');
+	});
+	$('.table-grid .body').each(function() {
+		$(this).children('tr:even').addClass('row-odd');
+		$(this).children('tr:odd').addClass('row-even');
+		$(this).children('tr').each(function() {
+			$(this).children('td:even').addClass('column-odd');
+			$(this).children('td:odd').addClass('column-even');
+		});
+	});
 	
 	/* INLINE ELEMENTS */
-	$('[datetime]').attr('title',function() {
-		return $(this).attr('datetime');
-	});
+//	$('[datetime]').attr('title',function() {
+//		return $(this).attr('datetime');
+//	});
 	/* hyperlinks */
 	$('.js-link[href]').hover(function() {
 		$(this).toggleClass('up dn');
@@ -102,9 +86,7 @@ $(document).ready(function() {
 	});
 	
 	/* MODS */
-	showOnHover($('.example, .demo'));
-	addLabel($('.example'), 'example');
-	addLabel($('.demo'), 'demo');
+//	showOnHover($('.example, .demo'));
 	/* block hyperlinks */
 	$('.js-block-link').hover(function() {
 		$(this).toggleClass('block-up block-dn',t_ani);
@@ -120,15 +102,21 @@ $(document).ready(function() {
 		$(this).parent().toggleClass('pos neg',t_ani);
 	});
 	/* code */
-	$('.lang-html').attr('title','HTML');
-	$('.lang-css').attr('title','CSS');
-	$('.lang-less').attr('title','LESS');
-	$('.lang-js').attr('title','JavaScript');
-	$('.lang-jq').attr('title','jQuery');
+//	$('.lang-html').attr('title','HTML');
+//	$('.lang-css').attr('title','CSS');
+//	$('.lang-less').attr('title','LESS');
+//	$('.lang-js').attr('title','JavaScript');
+//	$('.lang-jq').attr('title','jQuery');
+
 	/* math */
-	$('.latex').attr('title',function() {
-		return $(this).text();
+	$('data.latex[value]').html(function() {
+		return $(this).attr('value'); // for Mathjax rendering
 	});
+	$('data.latex[value]:after').css('content',''); // DOESN'T WORK!
+//	$('data.latex[value]').attr('title',function() {
+//		return $(this).attr('value'); // for people who want to see the LaTeX source
+//	});
+
 	/* special icons */
 	$('.icon-special').addClass('icon-special-clear');
 	$('.icon-special').hover(function() {
