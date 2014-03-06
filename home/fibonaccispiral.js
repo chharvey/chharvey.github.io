72dcc8c520d330aebdb6956214bfe509d184ab25
Chris <chrisharvey2pi@gmail.com>
2014-03-05 19:44:25 -0500

    home page ready for release

--- home/fibonaccispiral.js
+++ home/fibonaccispiral.js
@@ -1,29 +1,30 @@
 function p(n, length) {
-	var Phi = (-1 + Math.sqrt(5)) / 2;
+	// * returns the product of phi^n and length
+	var Phi = ((-1 + Math.sqrt(5)) / 2);
 //	var Phi = 0.618033988749894848;
 	return (length * Math.pow(Phi, n));
 }
 function squareheight(rectangle) {
-	// sets the height of each box link to its width
+	// * sets the height of each box link to its width
 	$(rectangle).css('height', function() {
 		return $(this).width();
 	});
 }
-function positionSquares(mw) {
-	//positions the squares in a Fibonacci spiral
-	var P1  = p(1,  mw);
-	var P2  = p(2,  mw);
-	var P3  = p(3,  mw);
-	var P4  = p(4,  mw);
-	var P5  = p(5,  mw);
-	var P6  = p(6,  mw);
-	var P7  = p(7,  mw);
-	var P8  = p(8,  mw);
-	var P9  = p(9,  mw);
-	var P10 = p(10, mw);
-	var P11 = p(11, mw);
-	var P12 = p(12, mw);
-	$('.buttons a').css('position','absolute'); // this is in JS and not in CSS: in case users have JS disabled but CSS not disabled, the position should be static
+function positionSquares(mainwidth) {
+	// * positions the squares in a Fibonacci spiral
+	var P1  = p(1,  mainwidth);
+	var P2  = p(2,  mainwidth);
+	var P3  = p(3,  mainwidth);
+	var P4  = p(4,  mainwidth);
+	var P5  = p(5,  mainwidth);
+	var P6  = p(6,  mainwidth);
+	var P7  = p(7,  mainwidth);
+	var P8  = p(8,  mainwidth);
+	var P9  = p(9,  mainwidth);
+	var P10 = p(10, mainwidth);
+	var P11 = p(11, mainwidth);
+	var P12 = p(12, mainwidth);
+	$('.spiralbutton').css('position','absolute'); // this is in JS and not in CSS: in case users have JS disabled but CSS not disabled, the position should be static
 	$('.a1').css('left',P2);
 	$('.c1').css('top',P2);
 	$('.d1').css('left',P3).css('top',P2+P5);
@@ -37,31 +38,32 @@ function positionSquares(mw) {
 	$('.l1').css('left',P3+P7+P11).css('top',P2+P6+P10);
 }
 function bigtext() {
-	// sets a proportional font size for each box link
+	// * sets a proportional font size for each box link
 	$('.buttontext').css('font-size', function () {
 		return $(this).parents('a').height() / 4 + 'px';
 	});
-	// vertically aligns the font in each box link
+	// * vertically aligns the font in each box link
 	$('.buttontext').css('top',function () {
 		return ($(this).parents('a').height() - $(this).height()) / 2 + 'px';
 	});
 }
 function positionSpiral(mw) {
-	// centers the group of box links on the page
-	$('.buttons').css('top', function() {
+	// * centers the group of box links on the page
+	$('.spiral').css('top', function() {
 		return ($(window).height() - p(1, mw)) * p(2, 1) + 'px';
 	});
 }
 function makepretty() {
+	// * brings it all together
 	var mainwidth = $('.main').width();
-	squareheight('.buttons a');
+	squareheight('.spiralbutton');
 	positionSquares(mainwidth);
 	bigtext();
 	positionSpiral(mainwidth);
 }
 
 $(document).ready(function() {
-//	showOnHover($('.buttons a'));
+	$('.webdev').append(newElemFilled('div','latex','\\(\\Phi\\)'));
 	makepretty();
 });
 $(window).resize(function() {
