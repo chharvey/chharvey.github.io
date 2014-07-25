50deee425a0a649592ce7659803f1fdbb3de2e1a
Chris Harvey <chrisharvey2pi@gmail.com>
2014-07-25 00:30:22 -0400

    added blurring effects to textboxes in the squares

--- home/fibonaccispiral.js
+++ home/fibonaccispiral.js
@@ -1,17 +1,11 @@
 function p(n, length) {
 	// * returns the product of phi^n and length
 	var Phi = ((-1 + Math.sqrt(5)) / 2);
-//	var Phi = 0.618033988749894848;
 	return (length * Math.pow(Phi, n));
 }
-function squareheight(rectangle) {
-	// * sets the height of the given rectangle to its width
-	$(rectangle).css('height', function() {
-		return $(this).width();
-	});
-}
-function positionSquares(mainwidth) {
+function positionSquares() {
 	// * positions the squares in a Fibonacci spiral
+	var mainwidth = $('main').width();
 	var P1  = p(1,  mainwidth);
 	var P2  = p(2,  mainwidth);
 	var P3  = p(3,  mainwidth);
@@ -40,34 +34,36 @@ function positionSquares(mainwidth) {
 	$('.Spiral > li:nth-child(11) > .Square').css('left', P3 + P7).css('top', P2 + P6 + P10);
 	$('.Spiral > li:nth-child(12) > .Square').css('left', P3 + P7 + P11).css('top', P2 + P6 + P10);
 }
-function bigtext() {
-	// * sets a proportional font size for each button
-	$('.Text').css('font-size', function() {
-		return $(this).parents('a').height() / 4 + 'px';
-	});
-	// * vertically aligns the text in each button
-	$('.Text').css('top', function() {
-		return ($(this).parents('a').height() - $(this).height()) / 2 + 'px';
-	});
-}
-function positionSpiral(mw) {
-	// * centers the group of buttons on the page
-	$('.Spiral').css('top', function() {
-		return ($(window).height() - p(1, mw)) * p(2, 1) + 'px';
-	});
-}
 function makepretty() {
-	// * brings it all together
-	var mainwidth = $('main').width();
-	squareheight('.Square');
-	positionSquares(mainwidth);
-	bigtext();
-	positionSpiral(mainwidth);
+	var user_has_CSS_enabled = true;
+	if (user_has_CSS_enabled) {
+		$('.Square').css('height', function() {
+			// * sets the height of the given rectangle to its width
+			return $(this).width();
+		});
+		$('.Text').css('font-size', function () {
+			// * sets a proportional font size for each square (dependent on square height)
+			return $(this).parents('.Square').height() / 4 + 'px';
+		});
+		$('.Text').css('top', function () {;
+			// * vertically aligns the textbox in each square (depenedent on font-size)
+			return ($(this).parents('.Square').height() - $(this).height()) / 2 + 'px';
+		});
+		$('.Text').css('border-radius', function () {
+			// * adjust the border-radius of the textbox proportionally
+			return $(this).height() / 2 + 'px';
+		});
+		positionSquares();
+		$('.Spiral').css('top', function () {
+			// * centers the group of buttons on the page
+			return ($(window).height() - p(1, $('main').width())) * p(2, 1) + 'px';
+		});
+	}
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
 	makepretty();
 });
-$(window).resize(function() {
+$(window).resize(function () {
 	makepretty();
 });
