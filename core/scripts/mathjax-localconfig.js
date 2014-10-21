12f662830c4e0cf53db5564d327b0aa542ad0077
Chris Harvey <chrisharvey2pi@gmail.com>
2014-10-21 14:16:50 -0400

    untabbed /core/doc/, /core/images/, and /core/scripts/

--- core/scripts/mathjax-localconfig.js
+++ core/scripts/mathjax-localconfig.js
@@ -1,31 +1,31 @@
 MathJax.Hub.Config({
-	messageStyle: 'normal',
-	menuSettings: {
-		zoom: 'None',
-		zscale: '150%',
-	},
-	tex2jax: {
-		inlineMath:  [ ['\\(' , '\\)'], ],
-		displayMath: [ ['\\[' , '\\]'], ],
-		ignoreClass: 'main',
-		processClass: 'latex',
-	},
-	TeX: {
-		extensions: ['action.js'],
-		equationNumbers: {autoNumber: 'AMS'},
-		Macros: {
-//			bold: ['{\\bf #1}',1], // template
-			expand: ['{\\left\(\\toggle\{#1\}\{#2\}\\endtoggle\\right\)}',2],
-			l: ['{\\mathsf #1}',1], // propositional variables, predicates
-			xor: '\\nLeftrightarrow', // exclusive disjunction
-			NN: '{\\mathbb N}', // natural numbers
-			ZZ: '{\\mathbb Z}', // integers
-			QQ: '{\\mathbb Q}', // rational numbers
-			RR: '{\\mathbb R}', // real numbers
-		},
-	},
-	'HTML-CSS': {
-	},
+  messageStyle: 'normal',
+  menuSettings: {
+    zoom: 'None',
+    zscale: '150%',
+  },
+  tex2jax: {
+    inlineMath:  [ ['\\(' , '\\)'], ],
+    displayMath: [ ['\\[' , '\\]'], ],
+    ignoreClass: 'main',
+    processClass: 'latex',
+  },
+  TeX: {
+    extensions: ['action.js'],
+    equationNumbers: {autoNumber: 'AMS'},
+    Macros: {
+//      bold: ['{\\bf #1}',1], // template
+      expand: ['{\\left\(\\toggle\{#1\}\{#2\}\\endtoggle\\right\)}',2],
+      l: ['{\\mathsf #1}',1], // propositional variables, predicates
+      xor: '\\nLeftrightarrow', // exclusive disjunction
+      NN: '{\\mathbb N}', // natural numbers
+      ZZ: '{\\mathbb Z}', // integers
+      QQ: '{\\mathbb Q}', // rational numbers
+      RR: '{\\mathbb R}', // real numbers
+    },
+  },
+  'HTML-CSS': {
+  },
 });
 
 MathJax.Ajax.loadComplete('http://chharvey.github.io/core/scripts/mathjax-localconfig.js');
