42754bffb24742423a562a86d5a6dfd68c35ad94
Chris Harvey <chrisharvey2pi@gmail.com>
2014-11-24 14:09:52 -0500

    updated fibspiral home js file

--- home/fibonaccispiral.js
+++ home/fibonaccispiral.js
@@ -1,47 +1,32 @@
 /**
-  * Returns the product of phi^n and length
-  * @param  : n      : the power of phi
-  * @param  : length : the multiplier
-  * @return : phi^n * length
-  */
-function p(n, length) {
-  var Phi = ((-1 + Math.sqrt(5)) / 2);
-  return (length * Math.pow(Phi, n));
-}
-/**
   * Positions the squares in a Fibonacci spiral.
   */
 function positionSquares() {
-  var mainwidth = $('main').width();
-  var P1  = p(1,  mainwidth);
-  var P2  = p(2,  mainwidth);
-  var P3  = p(3,  mainwidth);
-  var P4  = p(4,  mainwidth);
-  var P5  = p(5,  mainwidth);
-  var P6  = p(6,  mainwidth);
-  var P7  = p(7,  mainwidth);
-  var P8  = p(8,  mainwidth);
-  var P9  = p(9,  mainwidth);
-  var P10 = p(10, mainwidth);
-  var P11 = p(11, mainwidth);
-  var P12 = p(12, mainwidth);
+  /**
+    * Returns the product of Phi^n and $('main').width(), where Phi ≈ 0.618034
+    * @param n      the power of phi
+    * @return       Phi^n * length
+    */
+  function p(n) {
+    return ($('main').width() * Math.pow(Util.PHI_INV, n));
+  }
 
   /* this is in JS and not in CSS: in case users have
   JS disabled but CSS not disabled, the position
   should be static (as specified in css file) */
   $('.Square').css('position', 'absolute');
 
-  $('.Spiral > li:nth-child( 1) > .Square').css('left', P2);
-  $('.Spiral > li:nth-child( 3) > .Square').css('top', P2);
-  $('.Spiral > li:nth-child( 4) > .Square').css('left', P3).css('top', P2 + P5);
-  $('.Spiral > li:nth-child( 5) > .Square').css('left', P3 + P6).css('top', P2);
-  $('.Spiral > li:nth-child( 6) > .Square').css('left', P3).css('top', P2);
-  $('.Spiral > li:nth-child( 7) > .Square').css('left', P3).css('top', P2 + P6);
-  $('.Spiral > li:nth-child( 8) > .Square').css('left', P3 + P7).css('top', P2 + P6 + P9);
-  $('.Spiral > li:nth-child( 9) > .Square').css('left', P3 + P7 + P10).css('top', P2 + P6);
-  $('.Spiral > li:nth-child(10) > .Square').css('left', P3 + P7).css('top', P2 + P6);
-  $('.Spiral > li:nth-child(11) > .Square').css('left', P3 + P7).css('top', P2 + P6 + P10);
-  $('.Spiral > li:nth-child(12) > .Square').css('left', P3 + P7 + P11).css('top', P2 + P6 + P10);
+  $('.Spiral > li:nth-child( 1) > .Square').css('left', p(2));
+  $('.Spiral > li:nth-child( 3) > .Square').css('top', p(2));
+  $('.Spiral > li:nth-child( 4) > .Square').css('left', p(3)).css('top', p(2) + p(5));
+  $('.Spiral > li:nth-child( 5) > .Square').css('left', p(3) + p(6)).css('top', p(2));
+  $('.Spiral > li:nth-child( 6) > .Square').css('left', p(3)).css('top', p(2));
+  $('.Spiral > li:nth-child( 7) > .Square').css('left', p(3)).css('top', p(2) + p(6));
+  $('.Spiral > li:nth-child( 8) > .Square').css('left', p(3) + p(7)).css('top', p(2) + p(6) + p(9));
+  $('.Spiral > li:nth-child( 9) > .Square').css('left', p(3) + p(7) + p(10)).css('top', p(2) + p(6));
+  $('.Spiral > li:nth-child(10) > .Square').css('left', p(3) + p(7)).css('top', p(2) + p(6));
+  $('.Spiral > li:nth-child(11) > .Square').css('left', p(3) + p(7)).css('top', p(2) + p(6) + p(10));
+  $('.Spiral > li:nth-child(12) > .Square').css('left', p(3) + p(7) + p(11)).css('top', p(2) + p(6) + p(10));
 }
 function makepretty() {
   var user_has_CSS_enabled = true; // it is most likely the case that if users have JS enabled then they also have CSS enabled
@@ -65,7 +50,7 @@ function makepretty() {
     positionSquares();
     /** centers the group of buttons on the page */
     $('.Spiral').css('top', function () {
-      return ($(window).height() - p(1, $('main').width())) * p(2, 1) + 'px';
+      return ($(window).height() - $('main').width() * Util.PHI_INV) * Math.pow(Util.PHI_INV, 2) + 'px';
     });
   }
 }
