c8a8939ae64b1c0b2eaf629f9a0daad0b3ede801
Chris Harvey <chrisharvey2pi@gmail.com>
2014-12-17 15:58:20 -0500

    removed java package info files

--- core/scripts/Java-Library/src/chh/math/set/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Contains a set of classes analogous to the fundamental mathematical objects such as set, ordered pair, function, and number types.
- */
-package chh.elmnts;
--- core/scripts/Java-Library/src/chh/util/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * A set of utility classes. The classes in this package cannot be instantiated
- * and contain only static fields and methods,
- * used in normal mathematical calculations and random generations.
- */
-package chh.util;
