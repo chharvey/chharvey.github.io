c23b463b1b215e9b621bb259c8be7cb0229954a0
Chris Harvey <chrisharvey2pi@gmail.com>
2014-12-18 17:40:58 -0500

    converted Cylinder.java to Cylinder.js

--- core/scripts/geo/Circle.js
+++ core/scripts/geo/Circle.js
@@ -1,10 +1,13 @@
 /**
-  * A circle is the set of points equidistant from a point. That distance is called the radius
+  * A circle is the set of points equidistant from a point.
   * @param r a Length: the radius of this circle
-  * @param u optional: a string representing the units of measurement
   */
 function Circle(r) {
   this.radius = r;
+
+  /** The circumference of this circle. */
   this.circumference = this.radius.scale(2 * Math.PI);
+
+  /** The area of this circle. */
   this.area = this.radius.square().scale(Math.PI);
 }
---
+++ core/scripts/geo/Cylinder.js
@@ -0,0 +1,21 @@
+/**
+  * A cylinder is a prism whose base is a circle.
+  * @param r a Length: the radius of this cylinder
+  * @param h a Length: the height of this cylinder
+  */
+function Circle(r, h) {
+  this.radius = r;
+  this.height = h;
+
+  /** The base face of this cylinder. */
+  this.base = new Circle(this.radius);
+
+  /** The side face of this cylinder. */
+  this.side = new Rectangle(this.base.circumference, this.height);
+
+  /** The surface area of this cylinder. */
+  this.surface_area = this.base.area.add(this.base.area).add(this.side.area);
+
+  /** The volume of this cylinder. */
+  this.volume = this.base.area.multiply(this.height);
+}
