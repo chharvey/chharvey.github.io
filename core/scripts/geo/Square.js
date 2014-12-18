01729045e03b9d44a20c945d89d7842bba2e81ac
Chris Harvey <chrisharvey2pi@gmail.com>
2014-12-18 18:34:55 -0500

    converted Square.java to Square.js

---
+++ core/scripts/geo/Square.js
@@ -0,0 +1,10 @@
+/**
+  * Theorem: A square is a rectangle with four congruent sides.
+  * Theorem A square is a rhombus with four right angles.
+  * This class uses the first definition: thus class Square extends class Rectangle
+  * @param a   a Length object: the side of this square
+  */
+function Square(a, ang) {
+  Rectangle.call(this, a, a);
+}
+Util.extend(Square, Rectangle);
