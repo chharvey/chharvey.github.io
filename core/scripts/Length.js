5652f431bb1a84d285fe043a4e9e019f49534a98
Chris Harvey <chrisharvey2pi@gmail.com>
2014-12-17 19:13:02 -0500

    added Area and Volume js classes

--- core/scripts/Length.js
+++ core/scripts/Length.js
@@ -1,6 +1,6 @@
 /**
   * A wrapper class representing a one-dimensional measurement.
-  * @param x this Length object represented as a number
+  * @param x this object represented as a non-negative number
   * @param u optional: a string representing the units of measurement
   */
 function Length(x, u) {
@@ -18,7 +18,7 @@ Length.prototype.toString = function () {
 /**
   * Adds another Length object to this Length object.
   * @param length the Length object to add to this object
-  * @return       a length equal to the sum of both lengths
+  * @return       a Length equal to the sum of both Lengths
   */
 Length.prototype.add = function (length) {
   if (this.units === length.units) return new Length(this.value + length.value, this.units);
@@ -28,10 +28,10 @@ Length.prototype.add = function (length) {
 /**
   * Returns a scale factor of this Length object.
   * If the scale factor is <1, returns a new Length "shorter" than this Length.
-  * If the scale factor is >1, returns a new Length "longer" than this Length.
-  * If the scale factor is 1, returns a new Length equivalent to this Length.
-  * @param k the scale factor
-  * @return the product of the length and the scale factor
+  * If the scale factor is >1, returns a new Length "longer" than  this Length.
+  * If the scale factor is =1, returns a new Length equivalent to  this Length.
+  * @param k a non-negative number: the scale factor
+  * @return  the product of this Length and the scale factor
   */
 Length.prototype.scale = function (k) {
   return new Length(this.value * k, this.units);
@@ -39,11 +39,11 @@ Length.prototype.scale = function (k) {
 
 /**
   * Multiplies another Length object by this Length object, returning Area.
-  * @param length the Length object to multiply to this object
-  * @return       an Area equal to the product of both lengths
+  * @param length the Length object to multiply by this object
+  * @return       an Area equal to the product of both Lengths
   */
 Length.prototype.multiply = function (length) {
-  if (this.units === length.units) return new Area(this.value * length.value, this.units + '²'); // U+00B2
+  if (this.units === length.units) return new Volume(this.value * length.value, this.units);
   else                             return '' + this + ' × ' + length;
 }
 
@@ -73,27 +73,4 @@ Length.conversion = {
   */
 Length.convert = function (x, fromunit, tounit) {
   return x * Length.conversion[fromunit][tounit];
-}
-
-/**
-  * Adds two lengths and returns a length. The paramaters may be numbers or Length objects.
-  * If both params are numbers, returns a length equivalent to the sum of those numbers.
-  * If both params are Length objects with the same unit, returns a length equivalent to the sum
-  * of those Length's values, with the correct unit.
-  * If one is a number and one is a Length, returns a string concatenating both params.
-  * @param a the first length
-  * @param b the second length
-  * @return  a length equal to the sum of both lengths
-  */
-Length.add = function (length1, length2) {
-  var a, b, u, v;
-
-  if      (length1 instanceof Length)   {a = length1.value; u = length1.units;}
-  else if (typeof length1 === 'number') {a = length1;       u = '';}
-
-  if      (length2 instanceof Length)   {b = length2.value; v = length2.units;}
-  else if (typeof length2 === 'number') {b = length2;       v = '';}
-
-  if (u === v) return new Length(a + b, u);
-  else         return '' + length1 + ' + ' + length2;
 }
