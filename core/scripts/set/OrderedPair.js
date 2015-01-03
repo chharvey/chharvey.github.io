0f3a426e0c0fa72eb084abba2b58211be4a7992e
Chris Harvey <chrisharvey2pi@gmail.com>
2015-01-02 23:54:52 -0500

    added OrderedTriple.js

--- core/scripts/set/OrderedPair.js
+++ core/scripts/set/OrderedPair.js
@@ -1,15 +1,10 @@
 /**
   * An OrderedPair is a set that encodes the order of its two coordinates.
-  * An ordered pair is written (a,b), where a and b are the coordinates.
+  * An ordered pair is written (a, b), where a and b are the coordinates.
   *
   * The order of these coordinates is relevant, that is,
   * `(new OrderedPair(a,b)).equals(new OrderedPair(b,a)) === false` UNLESS
   * `a.equals(b)`.
-  *
-  * An OrderedPair is similar to a {@see Tuple} with 2 components (a 2-tuple),
-  * however they are structurally different from a set-theoretic approach:
-  * The ordered pair (a,b) does not have the same elements as the 2-tuple ‹a,b›
-  * and thus they are not equal.
   *
   * `new OrderedPair(a,b)` is defined as `new Pair(new Singleton(a), new Pair(a,b))`
   * @extends   Pair
@@ -23,12 +18,10 @@ function OrderedPair(a, b) {
 }
 Util.extend(OrderedPair, Pair);
 
-OrderedPair.prototype.owns = function (x) {
-  return this.element1.equals(x) || this.element2.equals(x);
-}
-
 OrderedPair.prototype.equals = function (x) {
   return this.abscissa.equals(x.abscissa) && this.ordinate.equals(x.ordinate);
+  // fix this, it's wrong. there may be a set that is an ordered pair that is not an instance of
+  // the OrderedPair class
 }
 
 OrderedPair.prototype.toString = function () {
