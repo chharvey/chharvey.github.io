658bb1fe7d2f5a20805b3054c0439f1afe50ac0e
Chris Harvey <chrisharvey2pi@gmail.com>
2015-01-03 00:35:41 -0500

    removed unused files and cleand up java

--- core/scripts/Java-Library/src/chh/math/set/Set.java
+++ core/scripts/Java-Library/src/chh/math/set/Set.java
@@ -29,4 +29,28 @@ public abstract class Set {
         }
         return newArray;
     }
+    /**
+    * Transfers entries from a two-dimensional array into a one-dimensional array, one by one.
+    * @param cluster   the outer set with members that are other sets whose members are to be copied
+    * @return          a one-dimensional array
+    */
+    private static final Set[] union(Set cluster) {
+      /* Adds the cardinalities of each set in the collection */
+      int cells = 0;
+      //for (Set element : cluster) {} // can't do a for-each loop unless it is a 'Collection'
+      for (int i = 0; i < cluster.cardinality().getValue(); i++) {
+        cells += cluster.toArray()[i].cardinality().getValue();
+      }
+      Set[] union = new Set[cells]; // creates the new blank array with the correct number of cells
+
+      /* copies the elements from the collection's sets into the new array */
+      int nCell = 0; // an index for the new array
+      for (int i = 0; i < cluster.cardinality().getValue(); i++) {
+        for (int j = 0; j < cluster.toArray()[i].cardinality().getValue(); j++) {
+          union[nCell] = cluster.toArray()[i].toArray()[j];
+          nCell++;
+        }
+      }
+      return union;
+    }
 }
