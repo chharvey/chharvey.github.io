d3b623631068de2cabf5d43e51e738904362e652
Chris Harvey <chrisharvey2pi@gmail.com>
2015-08-11 22:14:56 -0400

    add dual-tone letter encoding

--- core/scripts/Util.js
+++ core/scripts/Util.js
@@ -8,6 +8,19 @@ Util.PHI = (1 + Math.sqrt(5)) / 2
 Util.PSI = (1 - Math.sqrt(5)) / 2
 Util.PHI_INV = 1 / Util.PHI
 Util.TAU = 2 * Math.PI
+Util.LETTER_ENCODINGS = {
+  DUAL_TONE : {
+    'A':2 , 'B':2 , 'C':2
+  , 'D':3 , 'E':3 , 'F':3
+  , 'G':4 , 'H':4 , 'I':4
+  , 'J':5 , 'K':5 , 'L':5
+  , 'M':6 , 'N':6 , 'O':6
+  , 'P':7 , 'Q':7 , 'R':7 , 'S':7
+  , 'T':8 , 'U':8 , 'V':8
+  , 'W':9 , 'X':9 , 'Y':9 , 'Z':9
+  }
+  dualTone : function (s) { return Util.LETTER_ENCODINGS.DUAL_TONE[s] }
+}
 
 
 /**
