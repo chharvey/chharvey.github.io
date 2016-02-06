f68c384677553c7cde4d10a806728511a14c7ac2
Chris Harvey <chrisharvey2pi@gmail.com>
2016-02-06 13:13:39 -0500

    convert 'mix' from static fn to instance fn; other minor edits

--- core/scripts/color.js
+++ core/scripts/color.js
@@ -97,6 +97,44 @@ Color.prototype.darken = function darken(p) {
 }
 
 /**
+ * Mixes (averages) another color with this color, with a given weight favoring this color.
+ * If `w == 1.0`, this method will return exactly this color.
+ * `w == 0.0` will return exactly the other color.
+ * `w == 0.5` (default if omitted) will result in a perfectly even mix.
+ * @param `color` required Color object; the second color
+ * @param `w`     optional number between 0.0 and 1.0, defaults to 0.5; the weight favoring this color
+ * @return        a mix of the two given colors
+ */
+Color.prototype.mix = function mix(color, w) {
+  w = (typeof w === 'number') ? w : 0.5
+  var r = Math.round(Util.average(this.red,   color.red,   w))
+    , g = Math.round(Util.average(this.green, color.green, w))
+    , b = Math.round(Util.average(this.blue,  color.blue,  w))
+  return new Color(r, g, b)
+}
+
+/**
+ * Returns the *contrast ratio* between two colors.
+ * More info can be found at
+ * https://www.w3.org/TR/WCAG/#contrast-ratiodef
+ * @param `color` required Color object; the second color to check
+ * @return        the contrast ratio of this color with the argument
+ */
+Color.prototype.contrastRatio = function contrastRatio(color) {
+  function relLum(c) {
+    function coef(p) {
+      return (p <= 0.03928) ? p/12.92 : Math.pow((p+0.055)/1.055,2.4)
+    }
+    return 0.2126*coef(c.red  /255)
+         + 0.7152*coef(c.green/255)
+         + 0.0722*coef(c.blue /255)
+  }
+  var lum1 = relLum(this)
+    , lum2 = relLum(color)
+  return (Math.max(lum1, lum2) + 0.05) / (Math.min(lum1, lum2) + 0.05)
+}
+
+/**
  * Returns a string representation of this color.
  * If `space === 'hsv'`, returns `hsv(h, s, v)`
  * If `space === 'hsl'`, returns `hsl(h, s, l)`
@@ -108,7 +146,8 @@ Color.prototype.darken = function darken(p) {
 Color.prototype.toString = function toString(space) {
   function toHex(n) {
     n = +n || 0
-    n = Util.bound(n, 0, 255)
+    // n = Util.bound(n, 0, 255)
+    n = Math.max(0, Math.min(n, 255))
     return '0123456789abcdef'.charAt((n - n % 16) / 16) + '0123456789abcdef'.charAt(n % 16)
   }
   if (space === 'hex') return '#' + toHex(this.red) + toHex(this.green) + toHex(this.blue)
@@ -116,6 +155,9 @@ Color.prototype.toString = function toString(space) {
   if (space === 'hsl') return 'hsl(' + this.hsl_hue + ', ' + this.hsl_sat + ', ' + this.hsl_lum + ')'
                        return 'rgb(' + this.red     + ', ' + this.green   + ', ' + this.blue    + ')'
 }
+
+
+
 
 /**
   * Returns a new Color object, given a string of the form `rgb(r,g,b)` or `rgb(r, g, b)`,
@@ -235,37 +277,12 @@ Color.typeCheck = function typeCheck(param) {
  * Mixes (averages) two colors, with a given weight favoring the first color.
  * If `w == 1.0`, this method will return `color1`. `w == 0.0`, will return `color2`.
  * `w == 0.5` (default if omitted) will result in a perfectly even mix.
+ * TODO NOTE this function is deprecated. Use `Color.prototype.mix()` instead.
  * @param `color1` required Color object; the first color
  * @param `color2` required Color object; the second color
  * @param `w`      optional number between 0.0 and 1.0, defaults to 0.5; the weight favoring the first color
  * @return         a mix of the two given colors
  */
 Color.mix = function mix(color1, color2, w) {
-  w = (typeof w === 'number') ? w : 0.5
-  var r = Math.round(Util.average(color1.red,   color2.red,   w))
-    , g = Math.round(Util.average(color1.green, color2.green, w))
-    , b = Math.round(Util.average(color1.blue,  color2.blue,  w))
-  return new Color(r, g, b)
-}
-
-/**
- * Returns the *contrast ratio* between two colors.
- * More info can be found at
- * https://www.w3.org/TR/WCAG/#contrast-ratiodef
- * @param `color1` required Color object; the first color
- * @param `color2` required Color object; the second color
- * @return         the contrast ratio of the two given colors
- */
-Color.contrastRatio = function contrastRatio(color1, color2) {
-  function relLum(color) {
-    function coef(p) {
-      return (p <= 0.03928) ? p/12.92 : Math.pow((p+0.055)/1.055,2.4)
-    }
-    return 0.2126*coef(color.red  /255)
-         + 0.7152*coef(color.green/255)
-         + 0.0722*coef(color.blue /255)
-  }
-  var lum1 = relLum(color1)
-    , lum2 = relLum(color2)
-  return (Math.max(lum1, lum2) + 0.05) / (Math.min(lum1, lum2) + 0.05)
+  return color1.mix(color2, w)
 }
