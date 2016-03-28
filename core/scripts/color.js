7178f80acab7efe087491d1b3b33523b3d6953d9
Chris Harvey <chrisharvey2pi@gmail.com>
2016-03-28 15:26:52 -0400

    update Color.prototype.mix fn defn (BREAKING!)

--- core/scripts/color.js
+++ core/scripts/color.js
@@ -97,19 +97,19 @@ Color.prototype.darken = function darken(p) {
 }
 
 /**
- * Mixes (averages) another color with this color, with a given weight favoring this color.
- * If `w == 1.0`, this method will return exactly this color.
- * `w == 0.0` will return exactly the other color.
+ * Mixes (averages) another color with this color, with a given weight favoring that color.
+ * If `w == 0.0`, this method will return exactly this color.
+ * `w == 1.0` will return exactly the other color.
  * `w == 0.5` (default if omitted) will result in a perfectly even mix.
  * @param `color` required Color object; the second color
- * @param `w`     optional number between 0.0 and 1.0, defaults to 0.5; the weight favoring this color
+ * @param `w`     optional number between 0.0 and 1.0, defaults to 0.5; the weight favoring the other color
  * @return        a mix of the two given colors
  */
 Color.prototype.mix = function mix(color, w) {
   w = (typeof w === 'number') ? w : 0.5
-  var r = Math.round(Util.average(this.red,   color.red,   w))
-    , g = Math.round(Util.average(this.green, color.green, w))
-    , b = Math.round(Util.average(this.blue,  color.blue,  w))
+  var r = Math.round(Util.average(this.red,   color.red,   1-w))
+    , g = Math.round(Util.average(this.green, color.green, 1-w))
+    , b = Math.round(Util.average(this.blue,  color.blue,  1-w))
   return new Color(r, g, b)
 }
 
@@ -284,5 +284,5 @@ Color.typeCheck = function typeCheck(param) {
  * @return         a mix of the two given colors
  */
 Color.mix = function mix(color1, color2, w) {
-  return color1.mix(color2, w)
+  return color1.mix(color2, 1-w)
 }
