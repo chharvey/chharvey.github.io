87f0a83739d0ddbf03eb19b4b55f4086f8127aea
Chris Harvey <chrisharvey2pi@gmail.com>
2017-02-03 12:16:41 -0500

    add Home model & HOME tree

---
+++ _models/Home.class.js
@@ -0,0 +1,25 @@
+var Page = require('sitepage').Page
+
+module.exports = (function () {
+  // CONSTRUCTOR
+  function Home() {}
+
+  // STATIC MEMBERS
+  /**
+   * The set of main pages on this site.
+   * @type {Page}
+   */
+  Home.HOME = new Page({ name: 'Site', url: '/home/' })
+    .title('Chris Harvey’s Home Page')
+    .add(new Page({ name: 'About Me', url: 'about.html' })
+      .description('About Chris Harvey of Fairfax, VA.')
+      .add(new Page({ name: 'Copyright', url: 'copyright.html' }))
+    )
+    .add(new Page({ name: 'Mathematics', url: 'math.html' }))
+    .add(new Page({ name: 'Education'  , url: 'edu.html' }))
+    .add(new Page({ name: 'Music'      , url: 'music.html' }))
+    .add(new Page({ name: 'Swimming'   , url: 'swim.html' }))
+    .add(new Page({ name: 'Web'        , url: 'web.html' }))
+
+  return Home
+})()
