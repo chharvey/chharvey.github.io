0f9acaac3c6647249895fcf75251a460c2d3e498
Chris Harvey <chrisharvey2pi@gmail.com>
2017-11-02 19:28:03 -0400

    add gulp task pug:blog

--- gulpfile.js
+++ gulpfile.js
@@ -22,6 +22,20 @@ gulp.task('pug:home', function () {
     .pipe(gulp.dest('./home/'))
 })
 
+gulp.task('pug:blog', function () {
+  return gulp.src('blog/{classical-music,headings,layout,lists,math-moodle,page-relationships,semantic-punctuation,sort,workflow-git}.jade')
+    .pipe(pug({
+      basedir: './',
+      locals: {
+        Page    : require('sitepage').Page,
+        BlogPost: require('still-alive').BlogPost,
+        Home    : require('./_models/Home.class.js'),
+        entities: require('./blog/_models/entities.json'),
+      },
+    }))
+    .pipe(gulp.dest('./blog/'))
+})
+
 gulp.task('pug:resume', function () {
   return gulp.src('resume/resume.pug')
     .pipe(pug({
@@ -34,7 +48,7 @@ gulp.task('pug:resume', function () {
     .pipe(gulp.dest('./resume/'))
 })
 
-gulp.task('pug:all', ['pug:home','pug:resume'])
+gulp.task('pug:all', ['pug:home','pug:blog','pug:resume'])
 
 gulp.task('lessc:resume', function () {
   return gulp.src('resume/css/src/resume.less')
@@ -49,6 +63,7 @@ gulp.task('lessc:resume', function () {
 gulp.task('lessc:all', ['lessc:resume'])
 
 gulp.task('build:home', ['pug:home'])
+gulp.task('build:blog', ['pug:blog'])
 gulp.task('build:resume', ['pug:resume', 'lessc:resume'])
 
 gulp.task('build', ['pug:all', 'lessc:all'])
