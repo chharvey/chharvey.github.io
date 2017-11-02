a6b2c1503bd857e3eca9073ea6f0f8e9d1429a17
Chris Harvey <chrisharvey2pi@gmail.com>
2017-11-02 19:04:34 -0400

    add gulp task pug:home

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,25 @@ const pug          = require('gulp-pug')
 const less         = require('gulp-less')
 const autoprefixer = require('gulp-autoprefixer')
 
+gulp.task('pug:home', function () {
+  gulp.src('./index.jade')
+    .pipe(pug({
+      basedir: './',
+      locals: {
+        Home: require('./_models/Home.class.js'),
+      },
+    }))
+    .pipe(gulp.dest('./'))
+  return gulp.src('./home/{about,copyright,cover-letter,math,edu,music,swim,web}.jade')
+    .pipe(pug({
+      basedir: './',
+      locals: {
+        Home: require('./_models/Home.class.js'),
+      },
+    }))
+    .pipe(gulp.dest('./home/'))
+})
+
 gulp.task('pug:resume', function () {
   return gulp.src('resume/resume.pug')
     .pipe(pug({
@@ -15,7 +34,7 @@ gulp.task('pug:resume', function () {
     .pipe(gulp.dest('./resume/'))
 })
 
-gulp.task('pug:all', ['pug:resume'])
+gulp.task('pug:all', ['pug:home','pug:resume'])
 
 gulp.task('lessc:resume', function () {
   return gulp.src('resume/css/src/resume.less')
@@ -29,6 +48,7 @@ gulp.task('lessc:resume', function () {
 
 gulp.task('lessc:all', ['lessc:resume'])
 
+gulp.task('build:home', ['pug:home'])
 gulp.task('build:resume', ['pug:resume', 'lessc:resume'])
 
 gulp.task('build', ['pug:all', 'lessc:all'])
