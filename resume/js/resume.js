fd0e8996f2c12c39e5f6e4e618c03e78048caedf
Chris Harvey <chrisharvey2pi@gmail.com>
2017-11-27 01:26:01 -0500

    use x-award CE for dynamic content

--- resume/js/resume.js
+++ resume/js/resume.js
@@ -1,5 +1,3 @@
-const xjs   = require('extrajs')
-const Award = require('../class/Award.class.js')
 
 window.customElements.define('x-contactlink', require('./XContactLink.class.js'))
 window.customElements.define('x-skill', require('./XSkill.class.js'))
@@ -7,17 +5,24 @@ window.customElements.define('x-position', require('./XPosition.class.js'))
 window.customElements.define('x-city', require('./XCity.class.js'))
 window.customElements.define('x-award', require('./XAward.class.js'))
 
-let vctm_summary = {
-  "dates"  : "<time>2011</time>&ndash;<time>2014</time>",
-  "content": [
-    "<span itemscope=\"\" itemtype=\"http://schema.org/EducationalOrganization\">",
-      "<abbr class=\"c-Acro\" title=\"Virginia Council of Teachers of Mathematics\" itemprop=\"name\"><span class=\"c-Acro__First\">V</span>CTM</abbr>",
-      " Conferences, annually statewide (<time datetime=\"PT40H\">10 hr each</time>)",
-    "</span>"
-  ]
-}
+// let vctm_summary = {
+//   "dates"  : "<time>2011</time>&ndash;<time>2014</time>",
+//   "content": [
+//     "<span itemscope=\"\" itemtype=\"http://schema.org/EducationalOrganization\">",
+//       "<abbr class=\"c-Acro\" title=\"Virginia Council of Teachers of Mathematics\" itemprop=\"name\"><span class=\"c-Acro__First\">V</span>CTM</abbr>",
+//       " Conferences, annually statewide (<time datetime=\"PT40H\">10 hr each</time>)",
+//     "</span>"
+//   ]
+// }
 
-// copied from `Resume.class.js`:
-function _content(x) { return (xjs.Object.typeOf(x) === 'array') ? x.join('') : x }
-function newAward(d) { return new Award(d.dates, _content(d.content)) }
-$('#prof-dev > dl > dd:nth-of-type(1)').after(newAward(vctm_summary).view())
+$('#prof-dev > dl > dd:nth-of-type(1)').after(`
+  <x-award>
+    <dates><time>2011</time>&ndash;<time>2014</time></dates>
+    <text>
+      <span itemscope="" itemtype="http://schema.org/EducationalOrganization">
+        <abbr class="c-Acro" title="Virginia Council of Teachers of Mathematics" itemprop="name"><span class="c-Acro__First">V</span>CTM</abbr>
+        Conferences, annually statewide (<time datetime="PT40H">10 hr each</time>)
+      </span>
+    </text>
+  </x-award>
+`)
