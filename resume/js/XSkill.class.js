1aacd345fee5d01ff2edbf8f1d4196b889279c59
Chris Harvey <chrisharvey2pi@gmail.com>
2017-11-28 00:07:57 -0500

    use template <circle>

--- resume/js/XSkill.class.js
+++ resume/js/XSkill.class.js
@@ -11,19 +11,18 @@ class XSkill extends HTMLElement {
     frag.querySelector('.c-Label--skill'         ).textContent = Skill.LEVELS[this._level-1]
     frag.querySelector('[itemprop="bestRating"]' ).content     = Skill.LEVELS.length
     frag.querySelector('[itemprop="ratingValue"]').content     = this._level
+
     Skill.LEVELS.forEach(function (leveltext, index) {
       // NOTE: cannot use <template> in SVG elements
-      // <circle class="c-SkillViz__Marker {{ truthy }}" cx="{{ 3 * index }}" cy="0" r="1"/>
-      let circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
+      let circle = frag.querySelector('circle').cloneNode(true)
       // NOTE: these IDL properties are read-only for SVG elements, and return objects
       // instead of strings. While those objects are mutable, it’s too complicated
       // to access their settable properties, so it’s easier to call `.setAttribute()`.
-      circle.setAttribute('class', `c-SkillViz__Marker${(index <= this._level-1) ? ' c-SkillViz__Marker--true' : ''}`)
+      circle.setAttribute('class', circle.getAttribute('class').replace('{{ truthy }}', (index <= this._level-1) ? 'c-SkillViz__Marker--true' : ''))
       circle.setAttribute('cx'   , 3 * index)
-      circle.setAttribute('cy'   , 0)
-      circle.setAttribute('r'    , 1)
       frag.querySelector('g').appendChild(circle)
     }, this)
+    frag.querySelector('g').removeChild(frag.querySelector('g').firstElementChild) // remove the “template” `<circle>` element
 
     this.parentNode.appendChild(frag)
     this.remove()
